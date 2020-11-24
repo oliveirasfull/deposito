d637987120343858aab7245564848c47c6adaafe
oliveirasfull <lucas100cristhian@gmail.com>
2020-11-24 13:12:48 -0500

    sistema estavel ( casamento,nascimento e obito)+ coneção banco inicial com deposito previo

---
+++ banco.py
@@ -0,0 +1,85 @@
+import mysql.connector
+from caixa import CaixaDiario, depositoPrevio, servicoPrevio
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root",
+  database="CartorioDP"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM usuario")
+
+myresult = mycursor.fetchall()
+
+
+def login_bd(nome,senha):
+    for x in myresult:
+        if x[1] == nome and x[2] == senha:
+            return True
+
+
+def busca_nome_bd(nome_busca):
+
+    #sql = ("SELECT nomecrianca, DATE_FORMAT(datanasc,'%d-%m-%Y') AS datanascimento,  DATE_FORMAT(dataregistro,'%d-%m-%Y') AS dataDecla,livro,letra,folha,termo FROM nascimentos LIMIT 5")
+    sql= ("SELECT * FROM nascimentos")
+    mycursor.execute(sql)
+    
+    lista_nomes=[]
+    myresult = mycursor.fetchall()
+    
+    for x in myresult:
+      
+     
+        if nome_busca in x[7]:
+           
+            lista_nomes.append(x)
+           
+    return lista_nomes
+
+
+def busca_deposito():
+    sql= ("SELECT * FROM deposito_previo")
+    mycursor.execute(sql)
+
+    lista_depositos=[]
+    depositos =[]
+    lista_0 = []
+    
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        lista_depositos = depositoPrevio(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+        
+        depositos = depositoPrevio(lista_depositos.cod_deposito,lista_depositos.cpf_solicitante,lista_depositos.nome_solicitante,lista_depositos.tipo_documento,lista_depositos.criador,lista_depositos.data_criacao,lista_depositos.telefone,lista_depositos.usuario)
+        
+        lista_0.append(depositos)
+    return lista_0
+
+def cadastra_deposito():
+        
+        #insert into deposito_previo(criador,tipo_documento,cpf,data_criação,horario,Usuario_idUsuario) values ('Lucas','ESCRITURA PUBLICA DE COMPRA E VENDA','025-753-123.69','2020-11-23',null,1);
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+    
---
+++ caixa.py
@@ -0,0 +1,36 @@
+class CaixaDiario:
+    def __init__(self,usuario,cod_servico,valor,descricao,cpf,data):
+        self.usuario = usuario
+        self.cod_servico = cod_servico
+        self.valor = valor
+        self.descricao = descricao
+        self.cpf= cpf
+        self.data= data
+
+
+class depositoPrevio(object):
+    def __init__(self,cod_deposito,cpf_solicitante,nome_solicitante,tipo_documento,criador,data_criacao,telefone,usuario):
+        self.cod_deposito = cod_deposito
+        self.cpf_solicitante = cpf_solicitante
+        self.nome_solicitante = nome_solicitante
+        self.tipo_documento = tipo_documento
+        self.criador = criador
+        self.data_criacao = data_criacao,
+        self.telefone = telefone,
+        self.usuario = usuario
+      
+class servicoPrevio(depositoPrevio): 
+    def __init__(self,cod_deposito,cod_servico,descricao_servico,data_registro,data_entrega,user_inicio,user_fim,realizacao,valor,
+                cpf_solicitante,nome_solicitante,tipo_documento,criador,data_criacao):
+        super().__init__(cod_deposito,cpf_solicitante,nome_solicitante,tipo_documento,criador,data_criacao)
+        self.cod_servico = cod_servico
+        self.descricao_servico = descricao_servico
+        self.data_registro = data_registro
+        self.data_entrega = data_entrega
+        self.user_inicio = user_inicio
+        self.user_fim = user_fim
+        self.realizacao = realizacao
+        self.valor = valor
+
+      
+        
